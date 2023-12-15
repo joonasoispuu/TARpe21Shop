9faf239e7d91aa83a5c75e5896cda33687ae7498
mukim s <oispuu.joonas@gmail.com>
2023-12-15 21:57:52 +0200

    Added Cars model

---
+++ TARpe21Shop.Core/Dto/CarDto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TARpe21Shop.Core.Dto
+{
+    public class CarDto
+    {
+        public Guid Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+        public bool IsUsed { get; set; }
+
+
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+
+
+        public List<IFormFile> Files { get; set; }
+        public IEnumerable<FileToApiDto> FilesToApiDtos { get; set; } = new List<FileToApiDto>();
+    }
+}
